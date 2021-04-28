13eb8f0832032d6819697388ba39d21c41efdde5
Daniel <54057643+Chronos-deBohr@users.noreply.github.com>
2021-04-28 15:45:13 +0300

    апп

--- Plumb/js/app.js
+++ Plumb/js/app.js
@@ -42,4 +42,4 @@ if ($reviewsSlider.length) {
 }
 
 
-var preloader = document.getElementById("preloader_preload");function fadeOutnojquery(el){el.style.opacity = 1;var interpreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interpreloader);preloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(preloader);},100);};
+var preloader = document.getElementById("preloader_preload");function fadeOutnojquery(el){el.style.opacity = 1;var interpreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interpreloader);preloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(preloader);},300);};
